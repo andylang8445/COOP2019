c3d6a1551fd8d53990ee46be4aedb71f6be80559
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-10-09 18:20:30 -0400

    ㅁ

--- arduino_webserver/brakets/svg_creater/script.js
+++ arduino_webserver/brakets/svg_creater/script.js
@@ -1,3 +1,4 @@
+
 function return_val()
 {
    // var hw.addEventListener('hw',function(){
@@ -6,5 +7,12 @@ function return_val()
         document.getElementById("content").innerHTML = text_val;
         var link = document.getElementById('bg_address').value;
         document.getElementById('bg').$xlink=link;
+        
+        var text_element = document.getElementById("text");
+        text_content.setAttribute("type", "hidden");
+        var link_element = document.getElementById("bg_address");
+        link_element.setAttribute("type", "hidden");
+        var button_element = document.createElement("hw");
+        button_element.setAttribute("type", "hidden");
     //})
 }
