5ddff6d30809ab2159865fa09ddb02299448cc0a
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-10-09 22:47:54 -0400

    a

--- arduino_webserver/brakets/svg_creater/script.js
+++ arduino_webserver/brakets/svg_creater/script.js
@@ -1,18 +1,18 @@
+var text_val;
 
 function return_val()
 {
    // var hw.addEventListener('hw',function(){
         //document.writeln('hello world');
-        var text_val = document.getElementById('text').value;
-        document.getElementById("content").innerHTML = text_val;
+    
+        text_val = document.getElementById('text').value;
+        
         var link = document.getElementById('bg_address').value;
         document.getElementById('bg').$xlink=link;
         
-        var text_element = document.getElementById("text");
-        text_content.setAttribute("type", "hidden");
-        var link_element = document.getElementById("bg_address");
-        link_element.setAttribute("type", "hidden");
-        var button_element = document.createElement("hw");
-        button_element.setAttribute("type", "hidden");
     //})
+}
+function get_val()
+{
+    document.getElementById("content").innerHTML = text_val;
 }
