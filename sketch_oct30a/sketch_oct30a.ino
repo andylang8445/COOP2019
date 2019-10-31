333083cdeb8c4d99a393ca3029f15988982cf85a
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-10-31 19:37:33 -0400

    Add files via upload

--- sketch_oct30a/sketch_oct30a.ino
+++ sketch_oct30a/sketch_oct30a.ino
@@ -5,16 +5,18 @@ unsigned long time1;
 unsigned long time2 = 0;
 
 const char* ssid = "theHacksmith";
-const char* password = "iamspiderman3";
+const char* password = "iamspiderman4";
 
 const int relay = D1;
 const int input_hw = D5;
+const int outlet=D4;
 
 void setup() {
 
   Serial.begin(9600);
 
   pinMode(relay, OUTPUT);
+  pinMode(outlet, OUTPUT);
   Serial.print("Pinmode for relay D1 (");
   Serial.print(relay);
   Serial.print(" set to OUTPUT\n");
@@ -26,6 +28,8 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   digitalWrite(relay, LOW);
+  digitalWrite(input_hw, LOW);
+  digitalWrite(outlet, HIGH);
 }
 
 void toggle_relay()
@@ -42,12 +46,29 @@ void toggle_relay()
   }
 }
 void loop() {
-  time1 = millis();
+  /*time1 = millis();
 
-  if ((time1 - time2) >= 2500)
-  {
+    if ((time1 - time2) >= 2500)
+    {
     time2 = time1;
     toggle_relay();
+    }
+    else;*/
+  time1 = millis();
+  if ((time1 - time2) >= 128)
+  {
+    digitalWrite(outlet, HIGH);
+    time2 = time1;
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      toggle_relay();
+    }
+    else if (digitalRead(input_hw) == HIGH)
+    {
+      Serial.println("WIFI connection failed!");
+      Serial.print("Input pin is in HIGH\n");
+      toggle_relay();
+      digitalWrite(input_hw,LOW);
+    }
   }
-  else;
 }
