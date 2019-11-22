dfed2a2ca7158e4aff35aadfab2465a342bdcaf2
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-11-22 10:28:46 -0500

    .

---
+++ SQL_HTML/SQL table HTML/html_table_sql.php
@@ -0,0 +1,20 @@
+<?php
+	$conn = mysqli_connect("localhost","root","0000","test") or die("Connection Failed");
+	print "Connection Successful!"."<p>";
+	
+	print "html_sql_table.php loaded<br><br>";
+
+    print '<table style="width:100%""><tr><th>id</th><th>name</th><ht>birthday</th><th>age</th></tr>';
+	
+	$re=mysqli_query($conn,"SELECT * FROM index1 order by id;");
+	while($result=mysqli_fetch_array($re)){
+        print "<tr>";
+		print "<th>".$result[3]."</th>";
+		print "<th>".$result[0]."</th>";
+		print "<th>".$result[1]."</th>";
+		print "<th>".$result[2]."</th>";
+        print "</tr>";
+	}
+	mysqli_close($conn);
+	print "<br><a href='simple.html'>Main screen</a>";
+?>
