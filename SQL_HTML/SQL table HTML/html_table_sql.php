cba4c3af73badbf8bea6a81898009c2bb79dec38
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-11-27 10:11:08 -0500

    .

--- SQL_HTML/SQL table HTML/html_table_sql.php
+++ SQL_HTML/SQL table HTML/html_table_sql.php
@@ -12,18 +12,55 @@
     print '<table border="2"><tr><th>id</th><th>name</th><th>birthday</th><th>age</th></tr>';
 	
 	$tot=0;
+
+    $tot_result=array(
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+        array();
+    );
 	
 	$re=mysqli_query($conn,"SELECT * FROM index1 order by id;");
 	while($result=mysqli_fetch_array($re)){
         print "<tr>";
 		print "<td>".$result[3]."</td>";
+        $tot_result[$tot]["id"]=$result[3];
 		print "<td>".$result[0]."</td>";
+        $tot_result[$tot]["name"]=$result[0];
 		print "<td>".$result[1]."</td>";
+        $tot_result[$tot]["birthday"]=$result[1];
 		print "<td>".$result[2]."</td>";
+        $tot_result[$tot]["age"]=$result[2];
         print "</tr>";
 		$tot++;
 	}
 	print $tot." rows<br>";
+
+    print"<br>sorted data by name";
+    foreach ((array) $tot_result as $key => $value) {
+        $sort[$key] = $value['name'];
+    }
+
+    array_multisort($sort, SORT_ASC, $tot_result);
+    print_r($tot_result);
+
+
 	mysqli_close($conn);
 	print "<br><a href='table.html'>Main screen</a>";
 ?>
