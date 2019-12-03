5400becc46dd325cc6129f71dce3959a8a480a24
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-03 09:42:04 -0500

    Update html_table_sql.php

--- SQL_HTML/SQL table HTML/html_table_sql.php
+++ SQL_HTML/SQL table HTML/html_table_sql.php
@@ -51,11 +51,23 @@
         print "</tr>";
 		$tot++;
 	}
+
+
+    //array_multisort($sort, SORT_ASC, SORT_STRING,$tot_result);
+    print '<table border="2"><tr><th>id</th><th>name</th><th>birthday</th><th>age</th></tr>';
+	for($i=0;$i<=$tot;$i++){
+        print "<tr>";
+		print "<td>".$tot_result[$i][0]."</td>";
+		print "<td>".$tot_result[$i][1]."</td>";
+		print "<td>".$tot_result[$i][2]."</td>";
+		print "<td>".$tot_result[$i][3]."</td>";
+        print "</tr>";
+	}
 	print $tot." rows<br>";
 	$sort=array();
     print"<br>sorted data by name";
-    for($i=0;$i<$tot;$i++){
-        for($j=$i+1;$j<$tot+1;$j++){
+    for($i=0;$i<$tot-1;$i++){
+        for($j=$i+1;$j<$tot;$j++){
             if($tot_result[$i][1]>$tot_result[$j][1]){
                 for($k=0;$k<4;$k++){
                     $tmp=$tot_result[$i][$k];
