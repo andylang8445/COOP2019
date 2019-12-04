fe9e6b1258bf365924d46ab2f9d62525ec79e63d
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-04 10:20:10 -0500

    Update html_table_sql.php

--- SQL_HTML/SQL table HTML/html_table_sql.php
+++ SQL_HTML/SQL table HTML/html_table_sql.php
@@ -1,8 +1,8 @@
 <?php
     $sql_addr = "localhost";
-    $sql_user_name = "root";
-    $sql_pwd = "0000";
-    $sql_db_name = "test";
+    $sql_user_name="root";
+    $sql_pwd="0000";
+    $sql_db_name="test";
 
 	$conn = mysqli_connect($sql_addr,$sql_user_name,$sql_pwd,$sql_db_name) or die("Connection Failed");
 	print "Connection Successful!"."<p>";
@@ -13,7 +13,7 @@
 	
 	$tot=0;
 
-    $tot_result = array(
+    $tot_result=array(
         array(),
         array(),
         array(),
@@ -37,25 +37,48 @@
     );
 	
 	$re=mysqli_query($conn,"SELECT * FROM index1 order by id;");
-	print '<table border = "2"><tr><th>id</th><th>name</th><th>birthday</th><th>age</th></tr>';
-	while($result = mysqli_fetch_array($re)) {
-        print "<tr>";
-		print "<td>".$result[3]."</td>";
+	//print '<table border="2"><tr><th>id</th><th>name</th><th>birthday</th><th>age</th></tr>';
+	while($result=mysqli_fetch_array($re)){
+        //print "<tr>";
+		//print "<td>".$result[3]."</td>";
         $tot_result[$tot][0]=$result[3];
-		print "<td>".$result[0]."</td>";
+		//print "<td>".$result[0]."</td>";
         $tot_result[$tot][1]=$result[0];
-		print "<td>".$result[1]."</td>";
+		//print "<td>".$result[1]."</td>";
         $tot_result[$tot][2]=$result[1];
-		print "<td>".$result[2]."</td>";
+		//print "<td>".$result[2]."</td>";
         $tot_result[$tot][3]=$result[2];
-        print "</tr>";
+        //wprint "</tr>";
 		$tot++;
 	}
 
 
     //array_multisort($sort, SORT_ASC, SORT_STRING,$tot_result);
     print '<table border="2"><tr><th>id</th><th>name</th><th>birthday</th><th>age</th></tr>';
-	for($i = 0; $i < $tot; $i++){
+	for($i=0;$i<$tot;$i++){
+        print "<tr>";
+		print "<td>".$tot_result[$i][0]."</td>";
+		print "<td>".$tot_result[$i][1]."</td>";
+		print "<td>".$tot_result[$i][2]."</td>";
+		print "<td>".$tot_result[$i][3]."</td>";
+        print "</tr>";
+	}
+	$sort=array();
+    print"<br>sorted data by name";
+    for($i=0;$i<$tot-1;$i++){
+        for($j=$i+1;$j<$tot;$j++){
+            if($tot_result[$i][1]>$tot_result[$j][1]){
+                for($k=0;$k<4;$k++){
+                    $tmp=$tot_result[$i][$k];
+                    $tot_result[$i][$k]=$tot_result[$j][$k];
+                    $tot_result[$j][$k]=$tot;
+                }
+            }
+        }
+    }
+    //array_multisort($sort, SORT_ASC, SORT_STRING,$tot_result);
+    print '<table border="2"><tr><th>id</th><th>name</th><th>birthday</th><th>age</th></tr>';
+	for($i=0;$i<$tot;$i++){
         print "<tr>";
 		print "<td>".$tot_result[$i][0]."</td>";
 		print "<td>".$tot_result[$i][1]."</td>";
@@ -65,31 +88,6 @@
 	}
 
 	print $tot." rows<br>";
-	$sort = array();
-    print "<br>sorted data by name";
-    for($i = 0; $i < $tot - 1; $i++) { // Sort based on the name
-        for($j = ($i + 1); $j < $tot; $j++) {
-            if($tot_result[$i][1] > $tot_result[$j][1]) {
-                for($k = 0; $k < 4; $k++) {
-                    $tmp = $tot_result[$i][$k];
-                    $tot_result[$i][$k] = $tot_result[$j][$k];
-                    $tot_result[$j][$k] = $tot;
-                }
-            }
-        }
-    }
-
-    //array_multisort($sort, SORT_ASC, SORT_STRING,$tot_result);
-    print '<table border = "2"><tr><th>id</th><th>name</th><th>birthday</th><th>age</th></tr>';
-	for($i = 0; $i < $tot; $i++){
-        print "<tr>";
-		print "<td>".$tot_result[$i][0]."</td>";
-		print "<td>".$tot_result[$i][1]."</td>";
-		print "<td>".$tot_result[$i][2]."</td>";
-		print "<td>".$tot_result[$i][3]."</td>";
-        print "</tr>";
-	}
-
 
 	mysqli_close($conn);
 	print "<br><a href='table.html'>Main screen</a>";
