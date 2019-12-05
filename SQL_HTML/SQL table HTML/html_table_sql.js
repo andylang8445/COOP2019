1a44204d72b99ff90ccbd984722c79100f766ca0
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-05 18:26:40 -0500

    Update html_table_sql.js

--- SQL_HTML/SQL table HTML/html_table_sql.js
+++ SQL_HTML/SQL table HTML/html_table_sql.js
@@ -3,18 +3,9 @@ function myFunction() {
 }
    
 // Function to create the cookie 
-function createCookie(name, value, seconds) { 
-    var expires; 
-      
-    if (seconds) { 
-        var date = new Date(); 
-        date.setTime(date.getTime() + (seconds * 1000)); 
-        expires = "; expires=" + date.toGMTString(); 
-    } 
-    else { 
-        expires = ""; 
-    } 
-      
-    document.cookie = escape(name) + "=" +  
-        escape(value) + expires + "; path=/"; 
-} 
+function setCookie(cname, cvalue, extime) {
+  var d = new Date();
+  d.setTime(d.getTime() + (extime*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
