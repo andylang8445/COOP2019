dd3e0ab5854f8349ccc5f3067174b0bbf7979590
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-05 18:31:40 -0500

    Update html_table_sql.js

--- SQL_HTML/SQL table HTML/html_table_sql.js
+++ SQL_HTML/SQL table HTML/html_table_sql.js
@@ -1,36 +1,36 @@
 function myFunction() {
     alert("I am an alert box!");
 }
-   
+
 // Function to create the cookie 
 function setCookie_1() {
-  var d = new Date();
-  d.setTime(d.getTime() + (10*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = "cookie=1;" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + (10 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "cookie=1;" + expires + ";path=/";
+    alert("setCookie_1 activated!");
 }
 
 // Function to create the cookie 
 function setCookie_2() {
-  var d = new Date();
-  d.setTime(d.getTime() + (10*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = "cookie=2;" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + (10 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "cookie=2;" + expires + ";path=/";
 }
 
 // Function to create the cookie 
 function setCookie_3() {
-  var d = new Date();
-  d.setTime(d.getTime() + (10*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = "cookie=3;" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + (10 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "cookie=3;" + expires + ";path=/";
 }
 
 // Function to create the cookie 
 function setCookie_4() {
-  var d = new Date();
-  d.setTime(d.getTime() + (10*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = "cookie=4;" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + (10 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "cookie=4;" + expires + ";path=/";
 }
-
