03f1ca0628710c821ed32f0ceface0e37da90233
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-05 18:20:52 -0500

    .

--- SQL_HTML/SQL table HTML/html_table_sql.js
+++ SQL_HTML/SQL table HTML/html_table_sql.js
@@ -1,3 +1,20 @@
 function myFunction() {
     alert("I am an alert box!");
 }
+   
+// Function to create the cookie 
+function createCookie(name, value, seconds) { 
+    var expires; 
+      
+    if (seconds) { 
+        var date = new Date(); 
+        date.setTime(date.getTime() + (seconds * 1000)); 
+        expires = "; expires=" + date.toGMTString(); 
+    } 
+    else { 
+        expires = ""; 
+    } 
+      
+    document.cookie = escape(name) + "=" +  
+        escape(value) + expires + "; path=/"; 
+} 
