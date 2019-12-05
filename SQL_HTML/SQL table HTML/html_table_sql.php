8ef914fe251b1ee54cd51d4cea23a0c8d344c60e
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-05 18:31:08 -0500

    Update html_table_sql.php

--- SQL_HTML/SQL table HTML/html_table_sql.php
+++ SQL_HTML/SQL table HTML/html_table_sql.php
@@ -53,7 +53,7 @@
 	}
     echo '<script type="text/javascript" src="html_table_sql.js"></script>';
     //array_multisort($sort, SORT_ASC, SORT_STRING,$tot_result);
-    print '<table border="2"><tr><th onclick="myFunction();">id▼</th><th onclick="createCookie("cookie", "2", "10");">name</th><th>birthday</th><th>age</th></tr>';
+    print '<table border="2"><tr><th onclick="myFunction();">id▼</th><th onclick="setCookie_1();">name</th><th>birthday</th><th>age</th></tr>';
 	for($i=0;$i<$tot;$i++){
         print "<tr>";
 		print "<td>".$tot_result[$i][0]."</td>";
