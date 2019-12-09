4952e72e6995428065497be084d7591538880b18
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-09 08:45:32 -0500

    .

--- SQL_HTML/SQL table HTML/html_table_sql.js
+++ SQL_HTML/SQL table HTML/html_table_sql.js
@@ -1,30 +1,36 @@
-function myFunction() {
-    alert("I am an alert box!");
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[0];
+            y = rows[i + 1].getElementsByTagName("TD")[0];
+            //check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
 }
-
-// Function to create the cookie 
-function setCookie_1() {
-    var d = new Date();
-    d.setTime(d.getTime() + (10 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = "cookie=1;" + expires + ";path=/";
-    alert("setCookie_1 activated!");
-    location.reload();
-}
-
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "0000",
-  database: "test"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM index1 order by id;", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
