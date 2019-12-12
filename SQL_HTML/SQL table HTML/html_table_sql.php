49cf16216eca6602f57c678c303acdcc03fd9de3
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-11 19:33:00 -0500

    Update html_table_sql.php

--- SQL_HTML/SQL table HTML/html_table_sql.php
+++ SQL_HTML/SQL table HTML/html_table_sql.php
@@ -66,7 +66,7 @@ function toggle() {
     $sorted_element=0;//0:id, 1:name, 2:birthday, 3:age
     $sorted_increase_decrease=0;//0: increase, 1: decrease
     
-    print '<table id="myTable" border="2"><tr><th align="right" id="id_sec" onclick="sortTable1();$previous=$sorted_element;toggle();$sorted_element=0;">id &#62;</th><th align="right" id="name_sec" onclick="sortTable2();$previous=$sorted_element;toggle();$sorted_element=1;">name</th><th align="right" id="birthday_sec" onclick="sortTable3();$previous=$sorted_element;toggle();$sorted_element=2;">birthday</th><th align="right" id="age_sec" onclick="sortTable4();$previous=$sorted_element;toggle();$sorted_element=3;">age</th></tr>';
+    print '<table id="myTable" border="2"><tr><th align="right" id="id_sec" onclick="sortTable1();toggle();">id &#62;</th><th align="right" id="name_sec" onclick="sortTable2();toggle();">name</th><th align="right" id="birthday_sec" onclick="sortTable3();toggle();">birthday</th><th align="right" id="age_sec" onclick="sortTable4();toggle();">age</th></tr>';
     for($i=0;$i<$tot;$i++){
         print "<tr>" ; 
         print "<td align='right'>" .$tot_result[$i][0]."</td>"; 
