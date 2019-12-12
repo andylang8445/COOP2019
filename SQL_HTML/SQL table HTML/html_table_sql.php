3a7cfa53d6b72a5aa9febc5252d85ab09bfb9836
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2019-12-12 10:21:06 -0500

    Update html_table_sql.php

--- SQL_HTML/SQL table HTML/html_table_sql.php
+++ SQL_HTML/SQL table HTML/html_table_sql.php
@@ -57,7 +57,7 @@
     $sorted_element=0;//0:id, 1:name, 2:birthday, 3:age
     $sorted_increase_decrease=0;//0: increase, 1: decrease
     
-    print '<table id="myTable" border="2"><tr><th align="right"width="32"><button id="id_sec" onclick="sortTable1_1();" border="0" style="font-weight:bold;">id &#62;</button></th><th align="right" id="name_sec" onclick="sortTable2();" width="64">name</th><th align="right" id="birthday_sec" onclick="sortTable3();" width="72">birthday</th><th align="right" id="age_sec" onclick="sortTable4();">age</th></tr>';
+    print '<table id="myTable" border="2"><tr><th align="center" width="40"><button id="id_sec" onclick="sortTable1_1();">id &#62;</button></th><th align="right" id="name_sec" onclick="sortTable2();" width="64">name</th><th align="right" id="birthday_sec" onclick="sortTable3();" width="72">birthday</th><th align="right" id="age_sec" onclick="sortTable4();">age</th></tr>';
     for($i=0;$i<$tot;$i++){
         print "<tr>" ; 
         print "<td align='right'>" .$tot_result[$i][0]."</td>";
