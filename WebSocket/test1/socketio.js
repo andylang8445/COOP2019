46d7a9b0b26ae451d193064a122cee1334446c66
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-01-10 19:48:17 -0500

    .

--- WebSocket/test1/socketio.js
+++ WebSocket/test1/socketio.js
@@ -1,21 +1,23 @@
 var http = require('http');
 var fs = require('fs');
-var sockio = require('socketio');
+var socketio = require('socket.io');
 
 var server = http.createServer(function (req, res) {
     fs.readFile('index.html', function (err, data) {
-        res.WriteHead(200, {
+        res.writeHead(200, {
             'Content-Type': 'text/html'
         });
         res.end(data);
     });
 }).listen(8080, function () {
-    console.log('Running!');
+    console.log('Server on!');
 });
 
 var io = socketio.listen(server);
-io.sockets.on('connection', function (socket) {
-    socket.on('sMsg', function (data) {
-        io.socket.emit('rMsg', data);
+io.on('connection', function (socket) {
+    socket.on('sNum', function (data) {
+        console.log('Input detected');
+        var result = (data.num1) * (data.num2);
+        io.sockets.emit('getResult', result);
     });
 });
