87982b24d49757aed1bbc17fa73b34e9d1abdae7
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-01-14 20:10:35 -0500

    .

--- WebSocket/test1/socketio.js
+++ WebSocket/test1/socketio.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
 
@@ -19,5 +19,32 @@ io.on('connection', function (socket) {
         console.log('Input detected');
         var result = (data.num1) * (data.num2);
         io.sockets.emit('getResult', result);
+    });
+});*/
+
+
+// Node.js WebSocket server script
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const server = http.createServer();
+server.listen(9898);
+const wsServer = new WebSocketServer({
+    httpServer: server
+});
+wsServer.on('request', function (request) {
+    const connection = request.accept(null, request.origin);
+    connection.on('message', function (message) {
+        console.log('Received Message:', message.utf8Data);
+        const recievedMsg=message.utf8Data.split(":");
+        const n1 = parseInt(recievedMsg[0], 10);
+        const n2 = parseInt(recievedMsg[1], 10);
+        const result = n1 * n2;
+        console.log('n1:', n1);
+        console.log('n2:', n2);
+        console.log('result:', result);
+        connection.sendUTF(result);
+    });
+    connection.on('close', function (reasonCode, description) {
+        console.log('Client has disconnected.');
     });
 });
